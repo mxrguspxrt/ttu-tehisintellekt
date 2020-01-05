0fdf048ad0b95d1a4b38255cc3b939c797ad5868
Margus Pärt <margus@wave.ee>
2020-01-05 16:57:36 +0200

    fixed naming

--- lab7/resolution_task1.py
+++ lab7/resolution_task1.py
@@ -1,5 +1,10 @@
 
 
+control_table = """
+2 .
+. .
+"""
+
 table1 = """
 1 1 0
 ? 1 ?
@@ -20,14 +25,15 @@ table3 = """
 
 
 def main():
+    print_info(table=control_table)
     print_info(table=table1)
-    print_info(table=table2)
-    print_info(table=table3)
+    # print_info(table=table2)
+    # print_info(table=table3)
 
 
 def print_info(table=None):
     kb = KnowledgeBase(table=table)
-    kb.print_resolution()
+    kb.print_info()
 
 
 class KnowledgeBase:
@@ -37,26 +43,53 @@ class KnowledgeBase:
         print(table)
         stripped_table = table.strip()
         rows = stripped_table.split("\n")
-        self.rows = map(lambda row: row.split(" "), rows)
+        self.rows = list(map(lambda row: list(row.split(" ")), rows))
         self.rows_count = len(self.rows)
         self.columns_count = len(self.rows[0])
         self.elements_count = self.rows_count * self.columns_count
 
-    def print_resolution(self):
-        dnf = self.get_dnf()
-        print(dnf)
+    def print_info(self):
+        kb = self.get_kb()
+        print("KB: ", kb)
+
+        for row_index in range(0, self.rows_count):
+            for column_index in range(0, self.columns_count):
+                if self.rows[row_index][column_index] == "?":
+                    e_id = self.get_e_id(row_index, column_index)
+                    has_mine = self.get_has_mine(e_id=e_id, kb=kb)
+                    print("Resolution for element", e_id, "is that:",
+                          has_mine and "has a mine" or "does not have a mine")
+
         print("\n\n")
 
-    # dnf contains cnf-s for spefic elements
-    def get_dnf(self):
-        dnf = []
+    def get_has_mine(self, e_id=None, kb=None):
+        kb_without_duplicates = []
+        for dnf in kb:
+            if dnf not in kb_without_duplicates:
+                kb_without_duplicates.append(dnf)
+
+        return self.pl_resolution(kb=kb_without_duplicates, alpha=e_id) 
+    
+    def pl_resolution(self, kb=None, alpha=None):
+      clauses = []
+
+      for dnf in kb:
+        print("dnf:", dnf)
+
+    def pl_resolve(self, c1=None, c2=None):
+      pass
+
+
+
+    def get_kb(self):
+        kb = []
         for row_index in range(0, self.rows_count):
             for column_index in range(0, self.columns_count):
                 cnf = self.get_cnf(row_index=row_index,
                                    column_index=column_index)
                 if cnf:
-                    dnf.append(cnf)
-        return dnf
+                    kb.append(cnf)
+        return kb
 
     def get_cnf(self, row_index=None, column_index=None):
 
@@ -114,7 +147,7 @@ class KnowledgeBase:
 
             if val == "?" or val == ".":
                 selected_elements.append(e_id)
-        
+
         return self.cnf_sweeper(mines_around_count=mines_around_count, neighbors=selected_elements)
 
     def cnf_sweeper(self, mines_around_count=None, neighbors=None):
